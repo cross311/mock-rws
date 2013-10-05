89e3c83012aa720c4745ec1f42343d43b8628423
Connor Ross <cross@mdsol.com>
2013-10-05 14:22:28 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MockRws::Application.config.secret_key_base = 'ea2920354bce60d73c1295c62b9165f8501a4003a69260f22fa5463b2b0bf0f290fab9b752942c3f1111d50d996a83237f1876966226d9e14e735ffc81a3bb20'
